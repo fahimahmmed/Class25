93a88e3ff9796dc23db5e74ed9afe9e04b7cff24
Fahim Ahmmed <41587821+fahimahmmed@users.noreply.github.com>
2021-08-17 13:52:13 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+$(function(){
+  $('.s1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed:1000,
+    arrows:true,
+    fade:false,
+    dots:true,
+    prevArrow:'.left',
+    nextArrow:'.right',
+  });
+
+  $('.s2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed:1000,
+    fade:false,
+    arrows:false,
+    
+  });
+  $('.s3-overley').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed:1000,
+    fade:false,
+    arrows:false,
+    
+  });
+  $('.s4').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed:1000,
+    fade:false,
+    arrows:false,
+    
+  });
+  
+
+});
